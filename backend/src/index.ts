651c315f0807a6450f001723552b17267884d0e6
Zach Smith <zach.smith33@gmail.com>
2024-01-01 17:56:09 -0500

    remove upload route remove debug logging create translation controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,14 @@
 import express, { Request, Response, NextFunction, Express } from "express";
 import { connectWithRetry } from "./db/connect";
-import { upload, readFile } from "./fileupload/fileupload";
 import UserRouter from "./controllers/usercontroller";
-import * as Middleware from "./controllers/middleware";
+import TranslationRouter from "./fileupload/translationcontroller";
 
 const app: Express = express();
 app.use(express.json());
 connectWithRetry();
 
-app.post("/upload", Middleware.isLoggedIn, upload.single("file"), async (req, res, next) => {
-  const fileContents: any[] = await readFile((req as any).file.path);
-  res.json(fileContents);
-});
-
-console.log("testing!");
-
 app.use("/user", UserRouter);
+app.use("/translate", TranslationRouter);
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Root route is working!");
