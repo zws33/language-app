2865d0f81a65afce40c8e038483d57119a693026
Zach Smith <zach.smith33@gmail.com>
2024-01-11 13:36:09 -0500

    clean up environment variable definitions in docker

--- backend/src/controllers/translationController.ts
+++ backend/src/controllers/translationController.ts
@@ -3,20 +3,18 @@ const TranslationRouter = Router();
 TranslationRouter.post("/", async (req: Request, res: Response) => {
     let text = req.query.text as string;
     let targetLanguage = req.query.target_lang as string;
-    const apiUrl = process.env.TRANSLATION_SERVICE_URL as string;
-    const queryParams = {
+    const baseUrl = process.env.TRANSLATION_SERVICE_URL as string;
+    const queryString = new URLSearchParams({
         text: text,
         target_lang: targetLanguage,
-    };
-
-    const queryString = new URLSearchParams(queryParams).toString();
-    const fullUrl = `${apiUrl}?${queryString}`;
+    }).toString();
+    const fullUrl = `${baseUrl}?${queryString}`;
 
     try {
         const response = await fetch(fullUrl, {
             method: 'POST',
             headers: {
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': 'https://zwsmith.com'
             }
         });
 
