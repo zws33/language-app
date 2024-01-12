435f0e9cf6ed1ab1da2b9e5dc4c8f1f520f27137
Zach Smith <zach.smith33@gmail.com>
2024-01-12 13:09:43 -0500

    Update translation route to send translation data as json encoded body

--- backend/src/controllers/translationController.ts
+++ backend/src/controllers/translationController.ts
@@ -1,31 +1,16 @@
 import { Router, Request, Response } from "express";
+import { fetchTranslation } from "./fetchTranslation";
+
 const TranslationRouter = Router();
 TranslationRouter.post("/", async (req: Request, res: Response) => {
     let text = req.query.text as string;
     let targetLanguage = req.query.target_lang as string;
-    const baseUrl = process.env.TRANSLATION_SERVICE_URL as string;
-    const queryString = new URLSearchParams({
-        text: text,
-        target_lang: targetLanguage,
-    }).toString();
-    const fullUrl = `${baseUrl}?${queryString}`;
-
     try {
-        const response = await fetch(fullUrl, {
-            method: 'POST',
-            headers: {
-                'Access-Control-Allow-Origin': 'https://zwsmith.com'
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        res.send(data);
+        let data = await fetchTranslation(text, targetLanguage);
+        res.status(200).send(data);
     } catch (error) {
-        console.error('Error:', error);
+        res.status(500).send(error);
     }
 })
+
 export default TranslationRouter
