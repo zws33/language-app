58a30ed23a90484a5c3065a98276a79086c8d0aa
Zach Smith <zach.smith33@gmail.com>
2024-01-22 17:53:04 -0500

    Update .prettierrc and global.d.ts

--- backend/src/global.d.ts
+++ backend/src/global.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
       DATABASE_URL: string;
+      PORT: number;
       TRANSLATION_SERVICE_URL: string;
       CA_CERT: string | undefined;
     }
