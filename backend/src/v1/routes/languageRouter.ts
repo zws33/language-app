c6ea10fafdfce3dad0718458d9e5f5d5271f0686
Zach Smith <zach.smith33@gmail.com>
2024-02-06 19:13:25 -0500

    Update error handling for CRUD operations

--- backend/src/v1/routes/languageRouter.ts
+++ backend/src/v1/routes/languageRouter.ts
@@ -1,17 +1,28 @@
 import { Router } from 'express';
 import * as repository from '../data/repositories/languageRepository.js';
+import { isError } from '../../utils/Result.js';
 
 const languageRouter = Router();
 
 languageRouter.get('/', async (req, res) => {
   const result = await repository.getLanguages();
-  res.json(result);
+  if (isError(result)) {
+    res.status(500).json(result);
+    return;
+  } else {
+    res.json({ languages: result.data });
+  }
 });
 
 languageRouter.post('/', async (req, res) => {
   const languageCode = req.body.language_code as string;
-  const result = await repository.insertLanguage(languageCode);
-  res.json(result);
+  const result = await repository.insertLanguage([languageCode]);
+  if (isError(result)) {
+    res.status(500).json(result);
+    return;
+  } else {
+    res.json({ insertedLanguages: result.data });
+  }
 });
 
 export default languageRouter;
